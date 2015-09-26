64f38fbdec75b36b5a391aeb209264b0ca9ed367
azilnoor <azilnoor.salleh@vads.com>
2015-09-27 05:27:14 +0800

    Question 6
    Code for Q6

--- plot6.R
+++ plot6.R
@@ -6,32 +6,36 @@ SCC <- readRDS("Source_Classification_Code.rds")
 #Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips == 06037). 
 #Which city has seen greater changes over time in motor vehicle emissions?
 
+#Answer, 
+#no specific sources for motor vehicle SCC, therefore, gathered the subset in NEI corresponding to 'vehicle'. 
+#Vessels, airplane, agro equipment may not included in!
+
 # Gather the subset of the NEI data which corresponds to vehicles
 vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
 vehiclesSCC <- SCC[vehicles,]$SCC
 vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
 
 # Subset the vehicles NEI data by each city's fip and add city name.
-vehiclesBaltimoreNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
-vehiclesBaltimoreNEI$city <- "Baltimore City"
+baltvehicles <- vehiclesNEI[vehiclesNEI$fips=="24510",]
+baltvehicles$city <- "Baltimore City"
 
-vehiclesLANEI <- vehiclesNEI[vehiclesNEI$fips=="06037",]
-vehiclesLANEI$city <- "Los Angeles County"
+LAvehicles <- vehiclesNEI[vehiclesNEI$fips=="06037",]
+LAvehicles$city <- "Los Angeles County"
 
 # Combine the two subsets with city name into one data frame
-bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+CombDat <- rbind(baltvehicles,LAvehicles)
 
-png("plot6.png",width=480,height=480,units="px",bg="transparent")
+png("plot6.png",width=640,height=480,units="px")
 
 library(ggplot2)
 
-ggp <- ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+gplot <- ggplot(CombDat, aes(x=factor(year), y=Emissions, fill=city)) +
   geom_bar(aes(fill=year),stat="identity") +
   facet_grid(scales="free", space="free", .~city) +
-  guides(fill=FALSE) + theme_bw() +
-  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+  guides(fill=FALSE) + theme_grey() +
+  labs(x="Year", y=expression("Total Emission")) + 
   labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
 
-print(ggp)
+print(gplot)
 
 dev.off()
